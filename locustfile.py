994013fec1c1b84732381699a5aee96062085022
Benedict <holabenedict@gmail.com>
2023-09-14 17:17:56 +0700

    Add name to requests

--- locustfile.py
+++ locustfile.py
@@ -10,17 +10,17 @@ class ApiReadTest(HttpUser):
 
     @task
     def open_studies(self):
-        study_ids = self.client.get("/studies")
+        study_ids = self.client.get("/studies", name="/studies")
 
         for study_id in study_ids.json():
-            study = self.client.get(f"/studies/{study_id}")
+            study = self.client.get(f"/studies/{study_id}", name="/studies/{study_id}")
             series_ids = study.json()["Series"]
 
             for series_id in series_ids:
-                series = self.client.get(f"/series/{series_id}")
+                series = self.client.get(f"/series/{series_id}", name="/series/{series_id}")
 
                 for instance_id in series.json()["Instances"]:
-                    self.client.get(f"/instances/{instance_id}")
+                    self.client.get(f"/instances/{instance_id}", name="/instances/{instance_id}")
 
 
 class DownloadTest(HttpUser):
@@ -31,35 +31,35 @@ class DownloadTest(HttpUser):
         instance_ids = self.client.get("/instances")
 
         for instance_id in instance_ids.json():
-            self.client.get(f"/instances/{instance_id}/preview")
+            self.client.get(f"/instances/{instance_id}/preview", name="/instances/{instance_id}/preview")
 
     @task
     def download_image(self):
         instance_ids = self.client.get("/instances")
 
         for instance_id in instance_ids.json():
-            self.client.get(f"/instances/{instance_id}/file")
+            self.client.get(f"/instances/{instance_id}/file", name="/instances/{instance_id}/file")
 
     @task
     def download_series(self):
         series_ids = self.client.get("/series")
 
         for series_id in series_ids.json():
-            self.client.get(f"/series/{series_id}/archive")
+            self.client.get(f"/series/{series_id}/archive", name="/series/{series_id}/archive")
 
     @task
     def download_study(self):
         study_ids = self.client.get("/studies")
 
         for study_id in study_ids.json():
-            self.client.get(f"/studies/{study_id}/archive")
+            self.client.get(f"/studies/{study_id}/archive", name="/studies/{study_id}/archive")
 
     @task
     def download_patient(self):
         patient_ids = self.client.get("/patients")
 
         for patient_id in patient_ids.json():
-            self.client.get(f"/patients/{patient_id}/archive")
+            self.client.get(f"/patients/{patient_id}/archive", name="/patients/{patient_id}/archive")
 
 
 class AnonymizeTest(HttpUser):
@@ -67,7 +67,7 @@ class AnonymizeTest(HttpUser):
 
     @task
     def anonymize_studies(self):
-        study_ids = self.client.get("/studies")
+        study_ids = self.client.get("/studies", name="/studies")
 
         for study_id in study_ids.json():
-            self.client.post(f"/studies/{study_id}/anonymize", json={})
+            self.client.post(f"/studies/{study_id}/anonymize", json={ "Asynchronous": True }, name="/studies/{study_id}/anonymize")
