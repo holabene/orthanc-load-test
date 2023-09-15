844d9a5c35519b95f9966bae6ccee2debbd44db2
Benedict <holabenedict@gmail.com>
2023-09-15 11:39:39 +0700

    Pick random entity per user

--- locustfile.py
+++ locustfile.py
@@ -1,5 +1,5 @@
 from locust import HttpUser, task, between
-
+import random
 
 class ApiReadTest(HttpUser):
     wait_time = between(1, 5)
@@ -10,17 +10,18 @@ class ApiReadTest(HttpUser):
 
     @task
     def open_studies(self):
-        study_ids = self.client.get("/studies", name="/studies")
+        study_ids = self.client.get("/studies").json()
+        # pick a random study id from the list
+        study_id = random.choice(study_ids)
 
-        for study_id in study_ids.json():
-            study = self.client.get(f"/studies/{study_id}", name="/studies/{study_id}")
-            series_ids = study.json()["Series"]
+        study = self.client.get(f"/studies/{study_id}", name="/studies/{study_id}").json()
+        series_ids = study["Series"]
 
-            for series_id in series_ids:
-                series = self.client.get(f"/series/{series_id}", name="/series/{series_id}")
+        for series_id in series_ids:
+            series = self.client.get(f"/series/{series_id}", name="/series/{series_id}").json()
 
-                for instance_id in series.json()["Instances"]:
-                    self.client.get(f"/instances/{instance_id}", name="/instances/{instance_id}")
+            for instance_id in series["Instances"]:
+                self.client.get(f"/instances/{instance_id}", name="/instances/{instance_id}")
 
 
 class DownloadTest(HttpUser):
@@ -28,38 +29,38 @@ class DownloadTest(HttpUser):
 
     @task
     def preview_image(self):
-        instance_ids = self.client.get("/instances")
-
-        for instance_id in instance_ids.json():
-            self.client.get(f"/instances/{instance_id}/preview", name="/instances/{instance_id}/preview")
+        instance_ids = self.client.get("/instances").json()
+        # pick a random instance_id from the list
+        instance_id = random.choice(instance_ids)
+        self.client.get(f"/instances/{instance_id}/preview", name="/instances/{instance_id}/preview")
 
     @task
     def download_image(self):
-        instance_ids = self.client.get("/instances")
-
-        for instance_id in instance_ids.json():
-            self.client.get(f"/instances/{instance_id}/file", name="/instances/{instance_id}/file")
+        instance_ids = self.client.get("/instances").json()
+        # pick a random instance_id from the list
+        instance_id = random.choice(instance_ids)
+        self.client.get(f"/instances/{instance_id}/file", name="/instances/{instance_id}/file")
 
     @task
     def download_series(self):
-        series_ids = self.client.get("/series")
-
-        for series_id in series_ids.json():
-            self.client.get(f"/series/{series_id}/archive", name="/series/{series_id}/archive")
+        series_ids = self.client.get("/series").json()
+        # pick a random series_id from the list
+        series_id = random.choice(series_ids)
+        self.client.get(f"/series/{series_id}/archive", name="/series/{series_id}/archive")
 
     @task
     def download_study(self):
-        study_ids = self.client.get("/studies")
-
-        for study_id in study_ids.json():
-            self.client.get(f"/studies/{study_id}/archive", name="/studies/{study_id}/archive")
+        study_ids = self.client.get("/studies").json()
+        # pick a random study_id from the list
+        study_id = random.choice(study_ids)
+        self.client.get(f"/studies/{study_id}/archive", name="/studies/{study_id}/archive")
 
     @task
     def download_patient(self):
-        patient_ids = self.client.get("/patients")
-
-        for patient_id in patient_ids.json():
-            self.client.get(f"/patients/{patient_id}/archive", name="/patients/{patient_id}/archive")
+        patient_ids = self.client.get("/patients").json()
+        # pick a random patient_id from the list
+        patient_id = random.choice(patient_ids)
+        self.client.get(f"/patients/{patient_id}/archive", name="/patients/{patient_id}/archive")
 
 
 class AnonymizeTest(HttpUser):
@@ -67,7 +68,7 @@ class AnonymizeTest(HttpUser):
 
     @task
     def anonymize_studies(self):
-        study_ids = self.client.get("/studies", name="/studies")
-
-        for study_id in study_ids.json():
-            self.client.post(f"/studies/{study_id}/anonymize", json={ "Asynchronous": True }, name="/studies/{study_id}/anonymize")
+        study_ids = self.client.get("/studies").json()
+        # pick a random study id from the list
+        study_id = random.choice(study_ids)
+        self.client.post(f"/studies/{study_id}/anonymize", json={ "Asynchronous": True }, name="/studies/{study_id}/anonymize")
