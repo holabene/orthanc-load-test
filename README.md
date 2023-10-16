3bca1381fe3f01ba586b69737cb16ce9b958eefd
Bene <holabenedict@gmail.com>
2023-10-16 13:45:45 +0700

    Write test suite (#3)
    * WriteTest class
    
    * Create dicom file function
    
    * Upload and delete tests
    
    * Update README with poetry run and more examples
    
    * Safe delete
    
    * Ensure uploading produces new instance
    
    * Rename variable to upload
    
    * Set pixel data with 512x512 blank image
    
    * Create series, study and patient
    
    * Add usage warning
    
    * Add recommendation to upload studies

--- README.md
+++ README.md
@@ -6,22 +6,41 @@ This is a load test for Orthanc DICOM server. It is based on [Locust](http://loc
 ## Installation
 
 ```bash
-$ brew install python3
-$ brew install locust
+$ brew install poetry # or pip install poetry
+$ poetry install
 ```
 
 Refer to [Locust Documentation](http://docs.locust.io/en/latest/installation.html) for more details.
 
 ## Usage
 
+> **Warning**: The load test will create a lot of DICOM instances in the Orthanc server, and also perform delete operations on created instances.
+>
+> **It is recommended to use a separate Orthanc server for testing and not the production one.**
+>
+> Also recommended to upload a few DICOM studies to the Orthanc server before running the load test.
+
 ```bash
 $ # bring up the local services in docker-compose.yml
 $ docker compose up -d
 $ # run the load test
-$ locust -f locustfile.py --host=http://orthanc:orthanc@localhost:8042
+$ poetry run locust --host=http://orthanc:orthanc@localhost:8042
 ```
 
 Then open http://localhost:8089/ in your browser and start a test.
+
+To run headless, in this case, the test will run for 1 minute with 100 users and 10 users spawned per second:
+
+```bash
+$ poetry run locust --host=http://orthanc:orthanc@localhost:8042 --headless -u 100 -r 10 -t 1m
+```
+
+There is `locust-plugins` package available, which provides some useful features, such as limit iterations to run 1 task once:
+
+```bash
+$ poetry run locust --host=http://orthanc:orthanc@localhost:8042 --headless -u 1 -i 1
+```
+
 
 ## Screenshots
 
--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,13 @@
-from locust import HttpUser, task, between, events
+import tempfile
 import random
 import logging
+import os
+import numpy as np
+import uuid
+
+from locust import HttpUser, task, between, events
+from pydicom.dataset import FileDataset, FileMetaDataset
+from pydicom.uid import UID, generate_uid
 from datetime import datetime
 
 logger = logging.getLogger(__name__)
@@ -138,7 +145,8 @@ class SlowEndpointTest(HttpUser):
         study = self.client.get(f"/studies/{study_id}", name="/studies/{study_id}").json()
         study_instance_uid = study["MainDicomTags"]["StudyInstanceUID"]
         study_date = study["MainDicomTags"]["StudyDate"] if "StudyDate" in study["MainDicomTags"] else None
-        patient_id = study["PatientMainDicomTags"]["PatientID"] if "PatientID" in study["PatientMainDicomTags"] else None
+        patient_id = study["PatientMainDicomTags"]["PatientID"] \
+            if "PatientID" in study["PatientMainDicomTags"] else None
 
         # search for the study
         self.client.post("/tools/find", json={"Level": "Study", "Expand": True, "Query": {
@@ -150,13 +158,202 @@ class SlowEndpointTest(HttpUser):
         logger.info(f"Searched for study {study_instance_uid}, study date {study_date}, patient ID {patient_id}")
 
 
+def create_dicom_instance(patient_id, study_instance_uid=None, series_instance_uid=None):
+    """
+    Create a DICOM file
+    :return: DICOM file path
+    """
+    filename = tempfile.NamedTemporaryFile(suffix=".dcm", dir=".data/temp").name
+
+    file_meta = FileMetaDataset()
+    file_meta.MediaStorageSOPClassUID = UID("1.2.840.10008.5.1.4.1.1.7")  # Secondary Capture Image Storage
+    file_meta.MediaStorageSOPInstanceUID = generate_uid()
+    file_meta.ImplementationClassUID = '1.3.6.1.4.1.9590.100.1.0.100.5.5'
+
+    ds = FileDataset(filename, {}, file_meta=file_meta, preamble=b"\0" * 128)
+    ds.PatientName = "Test^Patient"
+    ds.PatientID = patient_id
+    ds.StudyInstanceUID = generate_uid() if study_instance_uid is None else study_instance_uid
+    ds.SeriesInstanceUID = generate_uid() if series_instance_uid is None else series_instance_uid
+    ds.SOPInstanceUID = generate_uid()
+    ds.Modality = "OT"
+    ds.ContentDate = datetime.now().strftime('%Y%m%d')
+    ds.ContentTime = datetime.now().strftime('%H%M%S')
+
+    # create a blank 512x512 black image
+    width = 512
+    height = 512
+    black_image = np.zeros((width, height), dtype=np.uint16)
+
+    ds.PixelSpacing = [1, 1]
+    ds.Rows = width
+    ds.Columns = height
+    ds.BitsStored = 16
+    ds.BitsAllocated = 16
+    ds.HighBit = 15
+    ds.PixelRepresentation = 0  # unsigned integer
+
+    # set pixel data
+    ds.PixelData = black_image.tobytes()
+
+    # save the file
+    ds.save_as(filename)
+
+    return filename
+
+
+def create_dicom_series(patient_id, study_instance_uid=None):
+    """
+    Create a DICOM series
+    :return: DICOM file path
+    """
+    # create a study if study_instance_uid is not provided
+    study_instance_uid = generate_uid() if study_instance_uid is None else study_instance_uid
+
+    # create series uid
+    series_instance_uid = generate_uid()
+
+    # create a DICOM series of 100 instances
+    filenames = []
+
+    for i in range(100):
+        filenames.append(create_dicom_instance(patient_id, study_instance_uid, series_instance_uid))
+
+    return filenames
+
+
+def create_dicom_study(patient_id):
+    """
+    Create a DICOM study
+    :return: DICOM file path
+    """
+    # create a study
+    study_instance_uid = generate_uid()
+
+    # create a DICOM study of 2 series
+    filenames = []
+
+    for i in range(2):
+        filenames.extend(create_dicom_series(patient_id, study_instance_uid))
+
+    return filenames
+
+
+def create_dicom_patient(patient_id):
+    """
+    Create a DICOM patient
+    :return: DICOM file path
+    """
+    # create a DICOM patient of 2 studies
+    filenames = []
+
+    for i in range(2):
+        filenames.extend(create_dicom_study(patient_id))
+
+    return filenames
+
+
+class WriteTest(HttpUser):
+    """
+    Test writing data to Orthanc
+    """
+    wait_time = between(10, 12)
+
+    def upload_file(self, filename):
+        with open(filename, "rb") as file:
+            upload = self.client.post("/instances", data=file, headers={
+                "Content-Type": "application/dicom"
+            }, name="/instances").json()
+
+        # delete the file
+        os.remove(filename)
+
+        logger.info(f"Uploaded file {filename} as instance ID {upload['ID']}, "
+                    f"ParentSeries {upload['ParentSeries']}, "
+                    f"ParentStudy {upload['ParentStudy']}, "
+                    f"ParentPatient {upload['ParentPatient']}")
+
+        if upload["Status"] != "Success":
+            raise Exception(f"Unexpected upload status {upload['Status']} for instance {upload['ID']}")
+
+        return upload
+
+    @task
+    def upload_and_delete_instance(self):
+        filename = create_dicom_instance(patient_id=str(uuid.uuid4()))
+        upload = self.upload_file(filename)
+
+        # delete the instance
+        self.client.delete(f"/instances/{upload['ID']}", name="/instances/{instance_id}")
+        logger.info(f"Deleted instance {upload['ID']}")
+
+    @task
+    def upload_and_delete_series(self):
+        filenames = create_dicom_series(patient_id=str(uuid.uuid4()))
+        uploads = []
+
+        for filename in filenames:
+            uploads.append(self.upload_file(filename))
+
+        # get the first upload
+        upload = uploads[0]
+
+        # delete the series
+        self.client.delete(f"/series/{upload['ParentSeries']}", name="/series/{series_id}")
+        logger.info(f"Deleted series {upload['ParentSeries']}")
+
+    @task
+    def upload_and_delete_study(self):
+        filenames = create_dicom_study(patient_id=str(uuid.uuid4()))
+        uploads = []
+
+        for filename in filenames:
+            uploads.append(self.upload_file(filename))
+
+        # get the first upload
+        upload = uploads[0]
+
+        # delete the study
+        self.client.delete(f"/studies/{upload['ParentStudy']}", name="/studies/{study_id}")
+        logger.info(f"Deleted study {upload['ParentStudy']}")
+
+    @task
+    def upload_and_delete_patient(self):
+        filenames = create_dicom_patient(patient_id=str(uuid.uuid4()))
+        uploads = []
+
+        for filename in filenames:
+            uploads.append(self.upload_file(filename))
+
+        # get the first upload
+        upload = uploads[0]
+
+        # delete the patient
+        self.client.delete(f"/patients/{upload['ParentPatient']}", name="/patients/{patient_id}")
+        logger.info(f"Deleted patient {upload['ParentPatient']}")
+
+
 @events.test_start.add_listener
 def on_test_start(**kwargs):
     # print current time in UTC
     logger.info(f"Test started at {datetime.utcnow()}")
+
+    # create temp folder if not exists
+    if not os.path.exists(".data/temp"):
+        os.makedirs(".data/temp")
+
+    # clean up temp files
+    for root, dirs, files in os.walk(".data/temp"):
+        for file in files:
+            os.remove(os.path.join(root, file))
 
 
 @events.test_stop.add_listener
 def on_test_stop(**kwargs):
     # print current time in UTC
     logger.info(f"Test stopped at {datetime.utcnow()}")
+
+    # clean up temp files
+    for root, dirs, files in os.walk(".data/temp"):
+        for file in files:
+            os.remove(os.path.join(root, file))
